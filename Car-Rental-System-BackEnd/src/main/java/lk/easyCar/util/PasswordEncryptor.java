b197c5bbe266a6f75ec79f7967f41080530f1f1b
Cha1552164 <chamithgithub>
2023-02-17 17:28:00 +0530

    updatec

--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/util/PasswordEncryptor.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/util/PasswordEncryptor.java
@@ -1,4 +1,27 @@
 package lk.easyCar.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class PasswordEncryptor {
+    private MessageDigest md;
+
+    public String getPassword(String pass) {
+        try {
+            md = MessageDigest.getInstance("MD5");
+            byte[] passBytes = pass.getBytes();
+            md.reset();
+            byte[] digested = md.digest(passBytes);
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < digested.length; i++) {
+                sb.append(Integer.toHexString(0xff & digested[i]));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(SecurityException.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
