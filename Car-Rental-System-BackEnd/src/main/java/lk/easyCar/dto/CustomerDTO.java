2010b02cd1539b6269ebb6532c9e0d2bd1354ae6
Cha1552164 <chamithgithub>
2023-02-17 14:17:16 +0530

    customer Property add

--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/dto/CustomerDTO.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/dto/CustomerDTO.java
@@ -1,5 +1,27 @@
 package lk.easyCar.dto;
 
-public class CustomerDTO {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String nic;
+    private String user_name;
+    private String password;
+    private String customer_name;
+    private String license_img;
+    private String nic_img;
+    private String address;
+    private String mobile;
+    private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date register_date;
 }
