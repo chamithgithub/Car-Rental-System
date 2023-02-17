0c394d4377434da31f26bef4389705e24378f7a1
Cha1552164 <chamithgithub>
2023-02-17 17:40:31 +0530

    CarReservation entity add

---
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/entity/CarReservation.java
@@ -0,0 +1,33 @@
+package lk.easyCar.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity(name = "car_reservation")
+public class CarReservation {
+    @Id
+    private String reserve_id;
+
+    private Date reserve_date;
+    private Date pick_up_date;
+    private Date return_date;
+    private Time pick_up_time;
+    private String pick_up_and_return_venue;
+    private int no_of_days;
+    private String bank_slip_img;
+    private String reservation_status;
+    private String driver_status;
+
+}
