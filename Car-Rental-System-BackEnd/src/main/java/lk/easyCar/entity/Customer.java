de4eaa1f77ab35653e8b32bfd13a344ef15f14a7
Cha1552164 <chamithgithub>
2023-02-17 14:25:23 +0530

    customer entity class implement

--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/entity/Customer.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/entity/Customer.java
@@ -1,5 +1,34 @@
 package lk.easyCar.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity(name = "customer")
 public class Customer {
+    @Id
+    private String nic;
+    private String user_name;
+    private String password;
+
+    private String customer_name;
+    private String license_img;
+    private String nic_img;
+    private String address;
+    private String mobile;
+    private String email;
+    private Date register_date;
+
 
 }
