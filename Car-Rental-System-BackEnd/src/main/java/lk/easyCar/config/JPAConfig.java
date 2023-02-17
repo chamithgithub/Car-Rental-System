14463fa3a8038d2c120f0575bda8a0c6fc0ecd72
Cha1552164 <chamithgithub>
2023-02-17 17:20:07 +0530

    passwordEncryptor add jpa

--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/config/JPAConfig.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/config/JPAConfig.java
@@ -1,5 +1,6 @@
 package lk.easyCar.config;
 
+import lk.easyCar.util.PasswordEncryptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -58,4 +59,8 @@ public class JPAConfig {
         return new JpaTransactionManager(emf);
     }
 
+    @Bean
+    public PasswordEncryptor passwordEncryptor() {
+        return new PasswordEncryptor();
+    }
 }
