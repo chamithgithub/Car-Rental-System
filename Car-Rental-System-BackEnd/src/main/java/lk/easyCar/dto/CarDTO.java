e94cc046df407a7e9012d03349e7a1237773d8a5
Cha1552164 <chamithgithub>
2023-02-17 18:01:41 +0530

    CarDTO dto add

---
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/dto/CarDTO.java
@@ -0,0 +1,32 @@
+package lk.easyCar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+
+public class CarDTO {
+    private String registration_no;
+    private String brand;
+    private String type;
+    private String transmission;
+    private String color;
+    private int no_of_passengers;
+    private int mileage;
+    private String fuel_type;
+    private int daily_rate;
+    private int monthly_rate;
+    private int free_km_for_day;
+    private int free_km_for_month;
+    private BigDecimal price_for_extra_km;
+    private BigDecimal waiver_payment;
+    private String status;
+
+}
