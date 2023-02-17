4a1dffd7b1bbe25f0cbd53a257381c379cdc54a3
Cha1552164 <chamithgithub>
2023-02-17 17:10:23 +0530

    CustomerService update

--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/service/CustomerService.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/service/CustomerService.java
@@ -1,4 +1,11 @@
 package lk.easyCar.service;
 
+import lk.easyCar.dto.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDTO customerDTO);
+
+
 }
