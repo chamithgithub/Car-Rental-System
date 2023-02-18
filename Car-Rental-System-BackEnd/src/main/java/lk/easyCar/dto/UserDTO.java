912739dea3d53200fb14b3231752b2bdc9651f4a
Cha1552164 <chamithgithub>
2023-02-18 12:07:28 +0530

    user Dto Immplement

--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/dto/UserDTO.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/dto/UserDTO.java
@@ -1,4 +1,16 @@
 package lk.easyCar.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class UserDTO {
+    private String customer_id;
+    private String user_name;
+    private String password;
 }
