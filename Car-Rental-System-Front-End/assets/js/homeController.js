56c5f1b718c351bb8401c5afc9e0c0c4de0c2745
Cha1552164 <chamithgithub>
2023-02-22 01:18:40 +0530

    customer login js implement

--- Car-Rental-System-Front-End/assets/js/homeController.js
+++ Car-Rental-System-Front-End/assets/js/homeController.js
@@ -1,4 +1,41 @@
 
+var baseUrl = "http://localhost:8080/Car_Rental_System/"
+
+//-------------------- Login
+$("#loginUserBtn").click(function () {
+
+    let userDTO = {
+        user_name: $("#login-page-user-name").val(),
+        password: $("#login-page-password").val()
+    }
+
+    $.ajax({
+        url: "http://localhost:8080/Car_Rental_System/controller/login",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(userDTO),
+        success: function (res) {
+            if (res.status === 200) {
+                if (res.message === ("Customer")) {
+                    customerLogin(res.data)
+                } else if (res.message === ("Driver")) {
+                    driverLogin(res.data)
+                } else if (res.message === ("Admin")) {
+                    adminLogin(res.data)
+                } else {
+                    alert(res.message)
+                }
+            }
+            $("#login-page-user-name").val("")
+            $("#login-page-password").val("")
+        },
+        error: function (ob) {
+            console.log(ob.responseJSON.message);
+        }
+    });
+})
+
+
 //-------------register page
 $(".getStartBtn").click(function () {
     $("#homePage").css('display', 'none')
