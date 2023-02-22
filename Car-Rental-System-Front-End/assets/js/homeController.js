1f4a1384738a72a506506c8de68b9b8dcd89a2fc
Cha1552164 <chamithgithub>
2023-02-22 14:47:05 +0530

    navigation contentU

--- Car-Rental-System-Front-End/assets/js/homeController.js
+++ Car-Rental-System-Front-End/assets/js/homeController.js
@@ -4,45 +4,73 @@ var baseUrl = "http://localhost:8080/Car_Rental_System/"
 //-------------------- Login
 $("#loginUserBtn").click(function () {
 
-    let userDTO = {
-        user_name: $("#login-page-user-name").val(),
-        password: $("#login-page-password").val()
-    }
+    // trail
+    $("#homeNavbar").css('display', 'block')
+        $("#loginPage").css("display", "none")
+        $("#admin").css("display", "block")
 
-    $.ajax({
-        url: "http://localhost:8080/Car_Rental_System/controller/login",
-        method: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(userDTO),
-        success: function (res) {
-            if (res.status === 200) {
-                if (res.message === ("Customer")) {
-                    customerLogin(res.data)
-                } else if (res.message === ("Driver")) {
-                    driverLogin(res.data)
-                } else if (res.message === ("Admin")) {
-                    adminLogin(res.data)
-                } else {
-                    alert(res.message)
-                }
-            }
-            $("#login-page-user-name").val("")
-            $("#login-page-password").val("")
-        },
-        error: function (ob) {
-            console.log(ob.responseJSON.message);
-        }
-    });
+        $("#adminDailySummary").css("display", "none")
+        $("#adminCars").css("display", "none")
+        $("#adminReservation").css("display", "none")
+        $("#adminDrivers").css("display", "none")
+        $("#adminCustomer").css("display", "none")
+        $("#adminPayments").css("display", "block")
+
+
+
+
+    // let userDTO = {
+    //     user_name: $("#login-page-user-name").val(),
+    //     password: $("#login-page-password").val()
+    // }
+    //
+    // $.ajax({
+    //     url: "http://localhost:8080/Car_Rental_System/controller/login",
+    //     method: "POST",
+    //     contentType: "application/json",
+    //     data: JSON.stringify(userDTO),
+    //     success: function (res) {
+    //         if (res.status === 200) {
+    //             if (res.message === ("Customer")) {
+    //                 customerLogin(res.data)
+    //             } else if (res.message === ("Driver")) {
+    //                 driverLogin(res.data)
+    //             } else if (res.message === ("Admin")) {
+    //                 adminLogin(res.data)
+    //             } else {
+    //                 alert(res.message)
+    //             }
+    //         }
+    //         $("#login-page-user-name").val("")
+    //         $("#login-page-password").val("")
+    //     },
+    //     error: function (ob) {
+    //         console.log(ob.responseJSON.message);
+    //     }
+    // });
 })
 
 
 //-------------register page
 $(".getStartBtn").click(function () {
-    $("#homePage").css('display', 'none')
-    $("#homeNavbar").css('display', 'none')
-    $("#loginPage").css('display', 'none')
+    // $("#homePage").css('display', 'none')
+    // $("#homeNavbar").css('display', 'none')
+    // $("#loginPage").css('display', 'none')
+    //
+    // $("#registerForm").css('display', 'block')
 
-    $("#registerForm").css('display', 'block')
+    // trail
+     $("#homePage").css('display', 'none')
+    $("#homeNavbar").css('display', 'block')
+    $("#loginPage").css("display", "none")
+    $("#admin").css("display", "block")
+
+    $("#adminDailySummary").css("display", "none")
+    $("#adminCars").css("display", "none")
+    $("#adminReservation").css("display", "none")
+    $("#adminDrivers").css("display", "none")
+    $("#adminCustomer").css("display", "none")
+    $("#adminPayments").css("display", "block")
 })
 
 //-------------Back Btn in Login & Register Page
@@ -63,6 +91,7 @@ $("#loginFormBtn").click(function () {
 
     $("#loginPage").css('display', 'block')
 })
+
 
 
 
