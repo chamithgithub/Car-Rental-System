51ed9add1c3f550ea03b13e63975fe347f6b3051
Cha1552164 <chamithgithub>
2023-02-26 13:52:58 +0530

    contentU

--- Car-Rental-System-Front-End/assets/js/homeController.js
+++ Car-Rental-System-Front-End/assets/js/homeController.js
@@ -5,71 +5,71 @@ var baseUrl = "http://localhost:8080/Car_Rental_System/"
 $("#loginUserBtn").click(function () {
 
     // trail
-    $("#loginPage").css("display", "none")
-    $("#admin").css("display", "block")
-    $("#adminNavBar").css("display", "block")
-
-    $("#adminDailySummary").css("display", "block")
-    $("#adminCars").css("display", "none")
-    $("#adminReservation").css("display", "none")
-    $("#adminDrivers").css("display", "none")
-    $("#adminCustomer").css("display", "none")
-    $("#adminPayments").css("display", "none")
-
-
+    // $("#loginPage").css("display", "none")
+    // $("#admin").css("display", "block")
+    // $("#adminNavBar").css("display", "block")
     //
-    // let userDTO = {
-    //     user_name: $("#login-page-user-name").val(),
-    //     password: $("#login-page-password").val()
-    // }
-    //
-    // $.ajax({
-    //     url: "http://localhost:8080/Car_Rental_System/controller/login",
-    //     method: "POST",
-    //     contentType: "application/json",
-    //     data: JSON.stringify(userDTO),
-    //     success: function (res) {
-    //         if (res.status === 200) {
-    //             if (res.message === ("Customer")) {
-    //                 customerLogin(res.data)
-    //             } else if (res.message === ("Driver")) {
-    //                 driverLogin(res.data)
-    //             } else if (res.message === ("Admin")) {
-    //                 adminLogin(res.data)
-    //             } else {
-    //                 alert(res.message)
-    //             }
-    //         }
-    //         $("#login-page-user-name").val("")
-    //         $("#login-page-password").val("")
-    //     },
-    //     error: function (ob) {
-    //         console.log(ob.responseJSON.message);
-    //     }
-    // });
+    // $("#adminDailySummary").css("display", "block")
+    // $("#adminCars").css("display", "none")
+    // $("#adminReservation").css("display", "none")
+    // $("#adminDrivers").css("display", "none")
+    // $("#adminCustomer").css("display", "none")
+    // $("#adminPayments").css("display", "none")
+
+
+
+    let userDTO = {
+        user_name: $("#login-page-user-name").val(),
+        password: $("#login-page-password").val()
+    }
+
+    $.ajax({
+        url: "http://localhost:8080/Car_Rental_System/controller/login",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(userDTO),
+        success: function (res) {
+            if (res.status === 200) {
+                if (res.message === ("Customer")) {
+                    customerLogin(res.data)
+                } else if (res.message === ("Driver")) {
+                    driverLogin(res.data)
+                } else if (res.message === ("Admin")) {
+                    adminLogin(res.data)
+                } else {
+                    alert(res.message)
+                }
+            }
+            $("#login-page-user-name").val("")
+            $("#login-page-password").val("")
+        },
+        error: function (ob) {
+            console.log(ob.responseJSON.message);
+        }
+    });
 
 })
 
 
 //-------------register page
 $(".getStartBtn").click(function () {
-    // $("#homePage").css('display', 'none')
-    // $("#homeNavbar").css('display', 'none')
-    // $("#loginPage").css('display', 'none')
-    //
-    // $("#registerForm").css('display', 'block')
+    $("#homePage").css('display', 'none')
+    $("#homeNavbar").css('display', 'none')
+    $("#loginPage").css('display', 'none')
+
+    $("#registerForm").css('display', 'block')
 
     // trail
 
-     $("#homePage").css('display', 'none')
-     $("#homeNavbar").css('display', 'none')
-
-
-        $("#customerReservation").css("display", "none")
-        $("#customerProfile").css("display", "none")
-
-        $("#customer").css("display", "block")
-    $("#customerNavbar").css("display", "block")
+    //  $("#homePage").css('display', 'none')
+    //  $("#homeNavbar").css('display', 'none')
+    //
+    //
+    //     $("#customerReservation").css("display", "none")
+    //     $("#customerProfile").css("display", "none")
+    //
+    //     $("#customer").css("display", "block")
+    // $("#customerNavbar").css("display", "block")
 
 
 
