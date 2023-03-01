47f0507071f6f242376e3511b976a334814d2273
Cha1552164 <chamithgithub>
2023-03-01 15:31:49 +0530

    when server start add auto admin up

--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/config/JPAConfig.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/config/JPAConfig.java
@@ -1,8 +1,11 @@
 package lk.easyCar.config;
 
 import lk.easyCar.util.PasswordEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -19,16 +22,30 @@ import javax.sql.DataSource;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "lk.easyCar.repo")
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
 
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
+//        LocalContainerEntityManagerFactoryBean bean= new LocalContainerEntityManagerFactoryBean();
+//        bean.setPackagesToScan("lk.easyCar.entity");
+//        bean.setDataSource(ds);
+//        bean.setJpaVendorAdapter(jpa);
+//        return bean;
+//    }
+
+    @Autowired
+    Environment env;
+
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
-        LocalContainerEntityManagerFactoryBean bean= new LocalContainerEntityManagerFactoryBean();
-        bean.setPackagesToScan("lk.easyCar.entity");
-        bean.setDataSource(ds);
-        bean.setJpaVendorAdapter(jpa);
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter adapter) {
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter(adapter);
+        bean.setDataSource(dataSource);
+        bean.setPackagesToScan(env.getRequiredProperty("entity.package.name"));
         return bean;
     }
+
 
     @Bean
     public DataSource dataSource(){
@@ -43,14 +60,23 @@ public class JPAConfig {
     }
 
     @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
-        HibernateJpaVendorAdapter va=new HibernateJpaVendorAdapter();
-        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
-        va.setDatabase(Database.MYSQL);
-        va.setGenerateDdl(true);
-        va.setShowSql(true);
-        return va;
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabasePlatform(env.getRequiredProperty("my.app.dialect"));
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        return adapter;
     }
+//    @Bean
+//    public JpaVendorAdapter jpaVendorAdapter(){
+//        HibernateJpaVendorAdapter va=new HibernateJpaVendorAdapter();
+//        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+//        va.setDatabase(Database.MYSQL);
+//        va.setGenerateDdl(true);
+//        va.setShowSql(true);
+//        return va;
+//    }
 
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
--- Car-Rental-System-BackEnd/src/main/java/lk/easyCar/config/WebRootConfig.java
+++ Car-Rental-System-BackEnd/src/main/java/lk/easyCar/config/WebRootConfig.java
@@ -1,5 +1,6 @@
 package lk.easyCar.config;
 
+
 import lk.easyCar.entity.Admin;
 import lk.easyCar.repo.AdminRepo;
 import lk.easyCar.util.PasswordEncryptor;
@@ -13,4 +14,25 @@ import org.springframework.core.env.Environment;
 @Import(JPAConfig.class)
 public class WebRootConfig {
 
+    @Autowired
+    AdminRepo adminRepo;
+
+    @Autowired
+    Environment env;
+
+    @Autowired
+    PasswordEncryptor passwordEncryptor;
+
+    // server start create admin account server
+    @Bean
+    public void createAdminAccount() {
+        Admin admin = new Admin();
+
+        String password = passwordEncryptor.doHashing(env.getProperty("admin.password"));
+
+        admin.setAdmin_id(env.getProperty("admin.id"));
+        admin.setPassword(password);
+        admin.setName(env.getProperty("admin.username"));
+        adminRepo.save(admin);
+    }
 }
