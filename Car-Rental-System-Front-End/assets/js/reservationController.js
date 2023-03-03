e74b285988b7c6bd5bde9ff97fbd1c4304efb8c1
Cha1552164 <chamithgithub>
2023-03-03 16:15:23 +0530

    Update Content

--- Car-Rental-System-Front-End/assets/js/reservationController.js
+++ Car-Rental-System-Front-End/assets/js/reservationController.js
@@ -1,5 +1,3 @@
-var reservation_Id;
-var data;
 
 $("#admin-pending-reservation").click(function () {
     $("#admin-reservation-title").css("display", "block")
@@ -20,6 +18,11 @@ $("#admin-today-pickups").click(function () {
 
     loadTodayPickUps()
 })
+
+
+var reservation_Id;
+var data;
+
 
 function loadPendingReservations() {
     $("#admin-reservation-table").empty();
@@ -65,4 +68,149 @@ function loadTodayPickUps() {
     });
 }
 
+$("#admin-view-reservation").click(function () {
+    if (reservation_Id == null) {
+        return
+    }
+    $.ajax({
+        url: baseUrl + "controller/reservation/getReservation/" + reservation_Id,
+        method: "GET",
+        success: function (resp) {
+            if (resp.status === 200) {
+                data = resp.data
+                setDataToViewReservationModal()
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+})
 
+function setDataToViewReservationModal() {
+    $("#admin-view-reservation-id").val(data.reserve_id)
+    $("#admin-view-reservation-driverNic").val(data.driver)
+    $("#admin-view-reservation-name").val(data.customer.nic)
+    $("#admin-view-reservation-pickUpDate").val(data.pick_up_date)
+    $("#admin-view-reservation-pickUpTime").val(data.pick_up_time)
+    $("#admin-view-reservation-venue").val(data.pick_up_and_return_venue)
+    $("#admin-view-reservation-returnDate").val(data.return_date)
+    $("#admin-view-reservation-reserveDate").val(data.reserve_date)
+    $("#admin-view-reservation-days").val(data.no_of_days)
+    $("#admin-view-reservation-vehicle").val(data.car.registration_no)
+    $("#admin-view-reservation-img").attr("src", baseUrl + data.bank_slip_img)
+
+    if (data.driver_status === "YES") {
+        getReservationDriver(data.reserve_id)
+    } else {
+        $("#admin-view-reservation-driverNic").val("Not Required");
+    }
+}
+
+$("#admin-update-reservation").click(function () {
+    if (reservation_Id == null) {
+        return
+    }
+    $.ajax({
+        url: baseUrl + "controller/reservation/getReservation/" + reservation_Id,
+        method: "GET",
+        success: function (resp) {
+            if (resp.status === 200) {
+                data = resp.data
+                setDataToUpdateReservationModal(resp.data)
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+})
+
+function setDataToUpdateReservationModal(data) {
+    $("#admin-update-reservation-id").val(data.reserve_id)
+    $("#admin-update-reservation-customer").val(data.customer.nic)
+    $("#admin-update-reservation-pickUpDate").val(data.pick_up_date)
+    $("#admin-update-reservation-pickUpTime").val(data.pick_up_time)
+    $("#admin-update-reservation-venue").val(data.pick_up_and_return_venue)
+    $("#admin-update-reservation-returnDate").val(data.return_date)
+    $("#admin-update-reservation-reserveDate").val(data.reserve_date)
+    $("#admin-update-reservation-vehicle").val(data.car.registration_no)
+    $("#admin-update-reservation-img").attr("src", baseUrl + data.bank_slip_img)
+
+    if (data.driver_status === "YES") {
+        getReservationDriver(data.reserve_id)
+    } else {
+        $("#admin-update-reservation-driver").prop('disabled', true);
+        $("#admin-update-reservation-driver").val("Not Required");
+    }
+}
+
+function updateOrDenyReservation(id, status, driver) {
+    console.log(id, status, driver)
+    $.ajax({
+        url: baseUrl + "controller/reservation?reserve_id=" + id + "&driver_id=" + driver + "&status=" + status,
+        method: "PUT",
+        success: function (resp) {
+            if (resp.status === 200) {
+                alert(resp.message)
+                loadPendingReservations();
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+$("#btnAcceptReservation").click(function () {
+
+    let driverField = $("#admin-update-reservation-driver").val();
+
+    var regExDriverNic = /^[0-9]{12}\b|[0-9]{10}[V]$/;
+    if (regExDriverNic.test(driverField)||driverField==="Not Required") {
+        $("#admin-update-reservation-driver").css('border', '2px solid blue');
+        if (data.driver_status === "YES") {
+            var id = data.reserve_id;
+            var status = "Accept"
+            var driver = $("#admin-update-reservation-driver").val();
+            updateOrDenyReservation(id, status, driver);
+        } else {
+            var id = data.reserve_id;
+            var status = "Accept"
+            var driver = "";
+            updateOrDenyReservation(id, status, driver);
+        }
+        $("#updateReservationModel").modal("toggle");
+    } else {
+        $("#admin-update-reservation-driver").css('border', '2px solid red');
+    }
+
+
+})
+
+$("#btnDenyReservation").click(function () {
+    let res = confirm("Do you really need to Cancel this Reservation ?");
+    if (res) {
+        var id = data.reserve_id;
+        var status = "Deny"
+        var driver = "";
+        updateOrDenyReservation(id, status, driver);
+        $("#updateReservationModel").modal("toggle");
+    }
+})
+
+function getReservationDriver(reserve_id) {
+    $.ajax({
+        url: baseUrl + "controller/driver/getSchedule/" + reserve_id,
+        method: "GET",
+        success: function (resp) {
+            if (resp.status === 200) {
+                $("#admin-view-reservation-driverNic").val(resp.data);
+                $("#admin-update-reservation-driver").val(resp.data);
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
